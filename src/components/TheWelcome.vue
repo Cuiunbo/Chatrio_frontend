318fec59eb28c01f682a3e020db0117c2d963799
Tokinodance <cuijb2000@outlook.com>
2023-03-14 23:41:34 +0800

    add login frontend

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -8,15 +8,27 @@ import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
-  <WelcomeItem>
+  <!-- <WelcomeItem>
     <template #icon>
       <DocumentationIcon />
     </template>
     <template #heading>Documentation</template>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+      <div class="login">
+      <h1>Login</h1>
+      <form @submit.prevent="login">
+        <label>
+          Username:
+          <input v-model="username" type="text" name="username" required>
+        </label>
+        <label>
+          Password:
+          <input v-model="password" type="password" name="password" required>
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      <div v-if="error" class="error">{{ error }}</div>
+    </div>
   </WelcomeItem>
 
   <WelcomeItem>
@@ -53,27 +65,32 @@ import SupportIcon from './icons/IconSupport.vue'
     you need more resources, we suggest paying
     <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
     a visit.
-  </WelcomeItem>
+  </WelcomeItem> -->
 
   <WelcomeItem>
     <template #icon>
       <CommunityIcon />
     </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
+    <template #heading>登录</template>
+    <div class="login-container">
+      <form @submit.prevent="login" class="login-form">
+        <div class="form-group">
+          <label for="username" class="form-label">邮箱:</label>
+          <input v-model="username" id="username" type="text" name="username" required class="form-input">
+        </div>
+        <div class="form-group">
+          <label for="password" class="form-label">密码:</label>
+          <input v-model="password" id="password" type="password" name="password" required class="form-input">
+        </div>
+        <div class="form-group">
+          <button type="submit" class="login-button">Log In</button>
+        </div>
+      </form>
+      <div v-if="error" class="error">{{ error }}</div>
+    </div>
   </WelcomeItem>
 
-  <WelcomeItem>
+  <!-- <WelcomeItem>
     <template #icon>
       <SupportIcon />
     </template>
@@ -82,5 +99,171 @@ import SupportIcon from './icons/IconSupport.vue'
     As an independent project, Vue relies on community backing for its sustainability. You can help
     us by
     <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+  </WelcomeItem> -->
 </template>
+
+<style>
+.login-container {
+display:block;
+justify-content: center;
+align-items: center;
+/* height: vh; */
+background-color: var(--color-background-mute);
+}
+
+.logo {
+margin-bottom: 2.5rem;
+}
+
+.form-container {
+width: 30rem;
+}
+
+.login-form {
+background-color: var(--color-background);
+padding: 2rem;
+border-radius: 8px;
+box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+
+.login-title {
+font-size: 2rem;
+margin-bottom: 2rem;
+color: var(--color-heading);
+}
+
+.form-group {
+margin-bottom: 1.5rem;
+}
+
+label {
+display: block;
+margin-bottom: 0.5rem;
+color: var(--color-text);
+}
+
+input[type="text"],
+input[type="password"] {
+width: 100%;
+padding: 0.75rem 1rem;
+border-radius: 4px;
+border: 1px solid var(--color-border);
+background-color: var(--color-background);
+color: var(--color-text);
+}
+
+.login-button {
+width: 100%;
+padding: 0.75rem;
+border-radius: 4px;
+background-color: var(--color-primary);
+color: var(--color-text-light);
+font-weight: 500;
+transition: background-color 0.2s ease;
+}
+
+.login-button:hover {
+background-color: var(--color-primary-dark);
+}
+
+@media (max-width: 600px) {
+.form-container {
+width: 100%;
+padding: 0 1.5rem;
+}
+}
+
+
+
+
+/* .login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: var(--color-bg-2);
+}
+
+.logo {
+  margin-bottom: 2.5rem;
+}
+
+.form-container {
+  width: 30rem;
+}
+
+.login-form {
+  background-color: var(--color-bg-1);
+  padding: 2rem;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+
+.login-title {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  color: var(--color-text);
+}
+
+.form-group {
+  margin-bottom: 1.5rem;
+}
+
+label {
+  display: block;
+  margin-bottom: 0.5rem;
+  color: var(--color-text);
+}
+
+input[type="text"],
+input[type="password"] {
+  width: 100%;
+  padding: 0.75rem 1rem;
+  border-radius: 4px;
+  border: 1px solid var(--color-border);
+  background-color: var(--color-bg-1);
+  color: var(--color-text);
+}
+
+.login-button {
+  width: 100%;
+  padding: 0.75rem;
+  border-radius: 4px;
+  background-color: var(--color-primary);
+  color: var(--color-text-light);
+  font-weight: 500;
+  transition: background-color 0.2s ease;
+}
+
+.login-button:hover {
+  background-color: var(--color-primary-dark);
+}
+
+@media (max-width: 600px) {
+  .form-container {
+    width: 100%;
+    padding: 0 1.5rem;
+  }
+} */
+/* .login-form {
+  background-color: var(--color-background-soft);
+  border: 1px solid var(--color-border);
+}
+
+.login-form label {
+  color: var(--color-heading);
+}
+
+.login-form input[type="text"],
+.login-form input[type="password"] {
+  background-color: var(--color-background-mute);
+  border: 1px solid var(--color-border);
+  color: var(--color-text);
+}
+
+.login-form button {
+  background-color: var(--color-background);
+  border: 1px solid var(--color-border);
+  color: var(--color-heading);
+} */
+</style>
