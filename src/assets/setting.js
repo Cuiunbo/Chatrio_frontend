b66dd543ef0095d746eab03924dd1364f47726aa
OzzyHelium <ozzyboogie@outlook.com>
2023-04-07 15:56:07 +0800

    set url

--- src/assets/setting.js
+++ src/assets/setting.js
@@ -1,8 +1,10 @@
 // 设置全局变量 -> 用于判断当前环境 -> 服务器环境还是本地环境
 export
 let set_Url;
-if (process.env.NODE_ENV === 'production') {
   set_Url = 'http://47.94.222.108:5000';
-} else {
-  set_Url = 'http://localhost:5000';
-}
+
+// if (process.env.NODE_ENV === 'production') {
+//   set_Url = 'http://47.94.222.108:5000';
+// } else {
+//   set_Url = 'http://localhost:5000';
+// }
